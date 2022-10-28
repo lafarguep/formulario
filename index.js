ff9b8f4d180b1b8148ecda8ad10c8582fc00a103
lafarguep <103155266+lafarguep@users.noreply.github.com>
2022-10-28 12:44:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+ let edad =prompt('ingrese su edad');
+
+ if(edad >= 18){
+
+    setTimeout(() => {
+        alert("Bienvenido")
+      }, 1000);
+     
+    }
+ else{
+    setTimeout(() => {
+             alert('Por favor, espera a tener 18 años')
+ }, 1000);
+ }
+
